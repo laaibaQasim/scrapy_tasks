c1ad8e058a21f78acc451c032799e7aa4e4c3e74
laaibaQasim <laibaqasim49@gmail.com>
2023-08-25 14:17:34 +0500

    removed extra classes, added required functions in coach_spider

--- task4_coach_scrawler/coach_spider.py
+++ task4_coach_scrawler/coach_spider.py
@@ -1,13 +1,13 @@
+import time
+import json
+import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-import time
 from task_4.items import CoachItem
-from task_4.field_setter import ItemSetter, ResponseHolder
 
 
 class CoachSpider(CrawlSpider):
     name = "coach_spider"
-
     allowed_domains = ["singapore.coach.com"]
     start_urls = ["https://singapore.coach.com/"]
 
@@ -27,26 +27,72 @@ class CoachSpider(CrawlSpider):
 
     def parse_item(self, response):
         start_time = int(time.time())
-        ResponseHolder().set_response(response)
+        coach_item = CoachItem()
 
-        field_setter = ItemSetter()
-        field_setter.set_fields()
-        fields = field_setter.get_fields()
+        coach_item["skus"] = self.get_skus(response)
+        coach_item["name"] = response.css("h1.page-title span::text").get()
+        coach_item["category"] = self.get_category(response)
 
-        item = CoachItem(
-            skus=fields["Skus"],
-            name=fields["Name"],
-            category=fields["Category"],
-            description=fields["Description"],
-            image_url=fields["ImageUrl"],
-            price=fields["Price"],
-            currency=fields["Currency"],
-            link=response.url,
-            url_original=response.url,
-            crawl_start_time=start_time,
-            spider_name=self.name,
-            brand="coach",
-            merch_info="Not Applicable",
-            care="Not Applicable",
+        coach_item["description"] = response.css(
+            "div.product.attribute.description div.value li::text"
+        ).getall()
+
+        coach_item["image_url"] = response.css(
+            'img[alt="main product photo"]::attr(src)'
+        ).get()
+
+        coach_item["price"] = (
+            "Not Applicable"
+            if (span := response.css(".product-info-price" " .price::text").get())
+            is None
+            else span.split("\xa0", 1)[1]
         )
-        yield item
+
+        coach_item["currency"] = (
+            "Not Applicable"
+            if (span := response.css(".product-info-price" " .price::text").get())
+            is None
+            else span.split("\xa0", 1)[0]
+        )
+
+        coach_item["link"] = response.url
+        coach_item["url_original"] = response.url
+        coach_item["crawl_start_time"] = start_time
+        coach_item["spider_name"] = self.name
+        coach_item["brand"] = "Coach"
+        coach_item["merch_info"] = "Not Applicable"
+        coach_item["care"] = "Not Applicable"
+
+        yield coach_item
+
+    def find_sku_dict(self, text):
+        pattern = r'"sku":\s*({[^}]*})'
+        match = re.search(pattern, text)
+
+        if match:
+            return match.group(1)
+        else:
+            return {}
+
+    def get_skus(self, response):
+        script_text = response.css('script:contains("Magento_Swatches/js/swatch-renderer")::text').get()
+
+        if not script_text:
+            sku_value = response.css('form[data-product-sku]::attr(data-product-sku)').get()
+            return "Not Applicable" if not sku_value else sku_value
+
+        sku_dict = json.loads(self.find_sku_dict(script_text))
+        skus = []
+
+        for key, value in sku_dict.items():
+            skus.append(value)
+
+        return "Not Applicable" if skus == [] else skus
+
+    def get_category(self, response):
+        ecomm_category_match = re.search(r'"ecomm_category":"([^"]+)"', response.text)
+        return (
+            lambda match: match.group(1).replace("/", "").replace("\\", ",")
+            if match
+            else None
+        )(ecomm_category_match)
